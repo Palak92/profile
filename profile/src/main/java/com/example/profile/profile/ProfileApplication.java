afbe652dab1e6326a8ab58048a05c0ed947475a9
Palak92 <plk.bnsl92@gmail.com>
2024-04-08 12:09:33 -0400

    Add config updater redis listener

--- profile/src/main/java/com/example/profile/profile/ProfileApplication.java
+++ profile/src/main/java/com/example/profile/profile/ProfileApplication.java
@@ -2,12 +2,38 @@ package com.example.profile.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class ProfileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileApplication.class, args);
+	}
+
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(ConfigChangeReciever receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	ConfigChangeReciever receiver() {
+		return new ConfigChangeReciever();
 	}
 
 }
